8c37ffe9c43d4d50fa80ac3928d3d6af3a26c4ad
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2021-10-26 14:07:32 +0200

    +

--- jsf1/src/main/java/com/antonio/jsf1/DatosPersonalesBean.java
+++ jsf1/src/main/java/com/antonio/jsf1/DatosPersonalesBean.java
@@ -19,19 +19,11 @@ public class DatosPersonalesBean {
 
     private String nombre;
     private Integer edad;
-    private final String DESTINO="destino.xhtml";
-    private final String DIFERENTE="diferente.xhtml";
+   
 
     @ManagedProperty(value = "#{param.pDestino}")
     private String pDestino;
 
-    public String getDESTINO() {
-        return DESTINO;
-    }
-
-    public String getDIFERENTE() {
-        return DIFERENTE;
-    }
 
     public Integer getEdad() {
         return edad;
@@ -57,20 +49,12 @@ public class DatosPersonalesBean {
         this.nombre = nombre;
     }
 
-    public String evaluarDestino() {
-
-        switch (pDestino) {
-            case "1":
-                return "destino.xhtml";
-
-            case "2":
-                return "diferente.xhtml";
-
-            
-        }
-        return "inicio.xhtml";
+    public String navegar1(){
+        return "valorDevuelto";
     }
-
+    public String navegar2(){
+        return "valorDevuelto";
+    }
     /**
      * Creates a new instance of DatosPersonalesBean
      */
